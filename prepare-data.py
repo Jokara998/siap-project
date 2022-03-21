5dc6a5bf24695632f212a161b6b6331ae2cb68f0
Stevan Rašković <lordsteva@hotmail.com>
2022-03-21 22:07:38 +0100

    add comments sentiment nalysis and  weekday

--- prepare-data.py
+++ prepare-data.py
@@ -1,9 +1,36 @@
+import datetime
 import json
+import ssl
+import statistics
+
+import nltk
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+
+from nltk.sentiment import SentimentIntensityAnalyzer
 
 from utils import get_json_data
 
+nltk.download([
+    "names",
+    "stopwords",
+    "state_union",
+    "twitter_samples",
+    "movie_reviews",
+    "averaged_perceptron_tagger",
+    "vader_lexicon",
+    "punkt",
+])
+
 
 def main():
+    sia = SentimentIntensityAnalyzer()
     posts = get_json_data('./datasets/dataset_instagram_posts.json')
     comments = get_json_data('./datasets/dataset_instagram_comments.json')
     profiles = get_json_data('./datasets/dataset_instagram_profiles.json')
@@ -11,6 +38,13 @@ def main():
     for post in posts:
         post['comments'] = [
             c for c in comments if c['shortCode'] == post['shortCode']]
+        polarities = [sia.polarity_scores(
+            c['text'])['compound'] for c in post['comments']]
+        post['avg_comment'] = statistics.mean(
+            polarities) if len(polarities) > 0 else 0
+        post['weekday'] = datetime.datetime.strptime(
+            post['timestamp'].split('.')[0], '%Y-%m-%dT%H:%M:%S').weekday()
+
         post['profile'] = [p for p in profiles if p['id'] == post['ownerId']][0]
         post['profile'].pop('latestPosts', None)
     with open('./datasets/dataset_all.json', "w", encoding='utf8') as file:
